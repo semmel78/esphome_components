33e3f68083eb33f0294efa386584e766dca14897
semmel78 <stefan@demmer.org>
2025-09-03 08:52:00 +0200

    Update __init__.py

--- components/ws2811_channels/__init__.py
+++ components/ws2811_channels/__init__.py
@@ -12,32 +12,33 @@ CONF_NUM_PIXELS = "num_pixels"
 CONF_NAME_PREFIX = "name_prefix"
 
 ns = cg.esphome_ns.namespace("ws2811_channels")
-Controller = ns.class_("WS2811ChannelsController", cg.Component)
+Controller   = ns.class_("WS2811ChannelsController", cg.Component)
 ChannelLight = ns.class_("WS2811ChannelLight", light.LightOutput)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        # Der Controller bekommt eine eigene ID
         cv.GenerateID(): cv.declare_id(Controller),
         cv.Required(CONF_STRIP_ID): cv.use_id(light.AddressableLightState),
         cv.Required(CONF_NUM_PIXELS): cv.int_range(min=1),
         cv.Optional(CONF_NAME_PREFIX, default="Pixel"): cv.string,
     }
-)
+).extend(cv.COMPONENT_SCHEMA)   # <<< WICHTIG
 
 async def to_code(config):
-    strip_state = await cg.get_variable(config[CONF_STRIP_ID])   # AddressableLightState*
-    num_pixels = config[CONF_NUM_PIXELS]
+    strip_state = await cg.get_variable(config[CONF_STRIP_ID])  # AddressableLightState*
+    num_pixels  = config[CONF_NUM_PIXELS]
     name_prefix = config[CONF_NAME_PREFIX]
     colors = ["Red", "Green", "Blue"]
 
-    # Controller mit eigener ID anlegen
+    # Controller mit ID erzeugen
     ctrl = cg.new_Pvariable(config[CONF_ID], Controller, strip_state, num_pixels)
-    await cg.register_component(ctrl)
 
-    # Für jeden Pixel & Kanal ein LightOutput anlegen (mit eigener, generierter ID)
+    # <<< WICHTIG: register_component mit config aufrufen
+    await cg.register_component(ctrl, config)
+
+    # Channel-Lights anlegen
     for p in range(num_pixels):
         for c, cname in enumerate(colors):
-            light_id = cg.new_id()  # generiere neue Variable-ID
+            light_id = cg.new_id()
             var = cg.new_Pvariable(light_id, ChannelLight, ctrl, p, c)
             await light.register_light(var, {CONF_NAME: f"{name_prefix} {p+1} - {cname}"})
